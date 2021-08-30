a69766499258d5b065f88cd43bbc83527e7e440a
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2021-08-30 08:54:47 +0200

    Update database.py

--- database.py
+++ database.py
@@ -6,11 +6,11 @@ load_dotenv()
 
 SELECT_POLLS = "SELECT * FROM polls;"
 SELECT_OPTIONS_IN_POLL = """
-SELECT options.option_text SUM(votes) FROM options
+SELECT options.option_text , SUM(votes.option_id) FROM options
 JOIN polls ON options.poll_id = polls.id  
 JOIN votes ON options.id = votes.option_id
 WHERE polls.id = %s
-GROUP BY options.text;"""
+GROUP BY options.option_text;"""
 
 connection = psycopg2.connect(os.environ.get("DATABASE_URI"))
 
